cac6b04bc0b57974789707e5f3c68300a7879124
Nicolas Gomes <nicolas-gomes2010@hotmail.fr>
2021-05-26 10:38:16 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
 });
+
+Route::resource('posts', 'PostController');
+
+Route::get('post_perso', 'PostController@liste');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
